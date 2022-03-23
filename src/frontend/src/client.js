56ef02a29a2d4a7ea8a7499a3bd7a9d07a607199
Dariusz Okonski <darek200180@gmail.com>
2022-03-23 12:31:23 +0000

    add file client.js

---
+++ src/frontend/src/client.js
@@ -0,0 +1,20 @@
+import fetch from 'unfetch';
+
+const checkStatus = (response) => {
+    if(response.ok) {
+        return response;
+    }
+
+    // convert non-2xx HTTP responses into errors
+    const error = new Error(response.statusText);
+    error.response = response;
+    return Promise.reject(error);
+}
+
+export const getAllStudents = () => {
+    fetch('http://localhost:8080/api/v1/students')
+        .then(checkStatus)
+        .catch(err => {
+            console.log(err);
+        })
+}
